1d236f79f958440a693ea60ac050e55a0a791222
Breno Wille <134011622+willehersomo@users.noreply.github.com>
2025-01-15 05:34:51 -0300

    Update D20_Glut.c

--- D20/D20_3D/D20_Glut.c
+++ D20/D20_3D/D20_Glut.c
@@ -12,6 +12,7 @@
 Este projeto utiliza a biblioteca stb_image para carregar as texturas. 
 A biblioteca é desenvolvida por Sean Barrett e disponibilizada sob licença pública de domínio ou MIT. 
 É uma ferramenta incrível para trabalhar com imagens de maneira simples em projetos C/C++.
+https://github.com/nothings/stb
 */
 
 typedef struct
